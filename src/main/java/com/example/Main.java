4242f301d84569b3e0041123a54d38e61c1dafe3
xiadnoring <timurtuty132@gmail.com>
2025-11-18 13:52:32 +0300

    UPD | task 3

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,15 +1,24 @@
 package com.example;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 public class Main {
     public static void main(String[] args) {
-        try (FileInputStream in = new FileInputStream("input.txt");
-                FileOutputStream out = new FileOutputStream("output.txt")) {
+        try {
+            FileInputStream in = new FileInputStream("input.txt");
+            FileOutputStream out = new FileOutputStream("output.txt");
             byte[] buffer = new byte[in.available()];
             in.read(buffer, 0, buffer.length);
+            String s = new String (buffer, "UTF-8");
+            String[] a = s.split(" ");
+            int a1 = Integer.parseInt(a[0]);
+            int a2 = Integer.parseInt(a[1]);
+            String b = String.format("%s",a1 / a2);
+            buffer = b.getBytes();
             out.write(buffer, 0, buffer.length);
-        } catch (IOException ex) {
+        } catch (Exception ex) {
+            System.out.println("Error was found");
             System.out.println(ex.getMessage());
         }
     }
